3ded7a628c60c2cc0646382a44ef3ed1d2c0f6fc
Aasif Ahmed <asifarslaan@gmail.com>
2024-09-03 13:13:09 +0530

    as on now

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -45,8 +46,8 @@ class User extends Authenticatable
         ];
     }
 
-    public function listings(){
-        return $this->hasMany(Listing::class);
+    public function listings():HasMany{
+        return $this->hasMany(\App\Models\Listing::class);
     }
 
 }
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,23 +1,22 @@
 <template>
     <header class="w-full border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800">
       <div class="container mx-auto">
-        <nav class="p-4 flex items-center justify-between">
+        <nav class="p-4 flex flex-wrap items-center justify-between">
           <div class="flex items-center text-lg font-medium">
             <Link href="/listing">Listings</Link>
           </div>
-          <div class="flex items-center text-indigo-600 dark:text-indigo-300 font-bold text-center">
+          <div class="flex items-center justify-center w-full sm:w-auto text-indigo-600 dark:text-indigo-300 font-bold text-center">
             <Link href="/listing">Larazillow</Link>
           </div>
           <div class="flex items-center gap-4">
-            <div v-if="user" class="flex items-center">
+            <div v-if="user" class="flex flex-col sm:flex-row items-center">
               <div class="text-sm text-gray-500 mx-2">{{ user.name }}</div>
-              <Link href="/listing/create" class="btn-primary">+ New Listing</Link>
-              <Link :href="route('logout')" method="DELETE" as="button" class="btn-primary mx-3">Logout</Link>
+              <Link href="/listing/create" class="btn-primary my-2 sm:my-0">+ New Listing</Link>
+              <Link :href="route('logout')" method="DELETE" as="button" class="btn-primary mx-3 my-2 sm:my-0">Logout</Link>
             </div>
-            <div v-else class="flex items-center gap-2">
-                <Link :href="route('user-account.create')">Register</Link>
-
-              <Link :href="route('login')">Sign-In</Link>
+            <div v-else class="flex flex-col sm:flex-row items-center gap-2">
+              <Link :href="route('user-account.create')" class="my-2 sm:my-0">Register</Link>
+              <Link :href="route('login')" class="my-2 sm:my-0">Sign-In</Link>
             </div>
           </div>
         </nav>
@@ -40,8 +39,6 @@
   const flashSuccess = computed(() => page.props.flash.success);
   const user = computed(() => page.props.user);
 
-
   const loginRoute = route('login'); // Ensure you have this route defined
   console.log(route('login')); // Check if this outputs the expected URL
-
   </script>
