dfa8133d2839a34b01cf5b5579bfa93d60b9a0fb
Aasif Ahmed <asifarslaan@gmail.com>
2024-09-03 11:37:40 +0530

    filters done
    filters done

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -11,22 +11,53 @@ class ListingController extends Controller
 {
     use AuthorizesRequests;
     public function __construct()
-{
-    // $this->authorizeResource(Listing::class, 'listing');
-    // $this->middleware('auth');
-}
+    {
+        // $this->authorizeResource(Listing::class, 'listing');
+        // $this->middleware('auth');
+    }
 
 
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
         // if (Auth::user()->can('view', Listing::class)) {
         //     abort('403');
         // }
+        $filters = $request->only([
+            'priceFrom',
+            'priceTo',
+            'beds',
+            'baths',
+            'areaFrom',
+            'areaTo'
+        ]);
+
+        // if ($filters['priceFrom']??false) {
+        //     $query->where('price', '>=', $filters['priceFrom']);
+        // }
+        // if ($filters['priceTo']??false) {
+        //     $query->where('price', '=<', $filters['priceTo']);
+        // }
+        // if ($filters['beds']??false) {
+        //     $query->where('beds',  $filters['beds']);
+        // }
+        // if ($filters['baths']??false) {
+        //     $query->where('baths',  $filters['baths']);
+        // }
+        // if ($filters['areaFrom']??false) {
+        //     $query->where('price', '>=', $filters['areaFrom']);
+        // }
+        // if ($filters['areaTo']??false) {
+        //     $query->where('price', '=<', $filters['areaTo']);
+        // }
         return inertia('Listing/Index', [
-            'listings' => Listing::all()
+            'filters' => $filters,
+            'listings' => Listing::mostRecent()
+                ->filter($filters)->paginate(10)
+                ->withQueryString()
+
         ]);
     }
 
